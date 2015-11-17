8a2b52ebdf6a7ac2bb54ea8a374a79b48bc7fbd3
Caleb Hicks <calebhicks@gmail.com>
2015-11-17 15:47:02 -0700

    Initial commit

---
+++ Journal-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Journal-C
+//
+//  Created by Caleb Hicks on 12/1/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
